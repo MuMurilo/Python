3a1a9b75cd2534e34a26467321099a0844238935
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2024-02-27 08:14:35 -0300

    ex03

---
+++ Exercicio_semana5/ativ1.py
@@ -0,0 +1,6 @@
+num1 = eval(input("Digite um número: "))
+num2 = eval(input("Digite um número: "))
+if num1  <  num2:
+    print("O menor número digitado é: ", num1)
+else:
+    print("O menor número digitado é: ", num2)
